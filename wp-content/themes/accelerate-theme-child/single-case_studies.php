845c4a2529967bdbd8d41d6818348da08fad3e42
Lesley <ljmerola@gmail.com>
2020-02-26 20:57:57 -0700

    getting content into single case studies templates

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -17,9 +17,42 @@ get_header(); ?>
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+        $services = get_field('services');
+        $client = get_field('client');
+        $link = get_field('site_link');
+        $image_1 = get_field('image_1');
+        $image_2 = get_field('image_2');
+        $image_3 = get_field('image_3');
+      ?>
+
+        <article class="case-study">
+          <aside class="case-study-sidebar">
+            <h2><?php the_title(); ?></h2>
+            <h4><?php echo $services; ?></h4>
+            <h4>Client: <?php echo $client; ?></h4>
+
+		 <?php the_content(); ?>
+
+         <p class="read-more-link"><a href="<?php echo $link; ?>">Site Link</a></p>
+      </aside>
+
+        <div class="case-study-images">
+          <?php if($image_1) { ?>
+            <img src="<?php echo $image_1"; ?> />
+          <?php } ?>
+
+          <?php if($image_2) { ?>
+            <img src="<?php echo $image_2"; ?> />
+          <?php } ?>
+
+          <?php if($image_3) { ?>
+            <img src="<?php echo $image_3"; ?> />
+          <?php } ?>
+        </div>
+     </article>
+  <?php endwhile; // end of the loop. ?>
+
+    </div><!-- .main-content -->
 
 	</div><!-- #primary -->
 
